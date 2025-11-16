468e71c2bf849261171d3af6040cdaf9d1de796c
OmVajrapaniHum <jakob.janzen80@gmail.com>
2025-11-16 11:12:52 +0100

    test1

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages # type: ignore
+
+setup(
+  name="tuxman",
+  version="0.1.0",
+  author="Jakob Janzen",
+  author_email="jakob.janzen80@gmail.com",
+  description="Linux automated installation and configuration tool",
+  packages=find_packages()
+  )
