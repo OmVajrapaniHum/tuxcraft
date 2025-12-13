b49f08b6b4bac689ec66f30430267251635225d5
OmVajrapaniHum <jakob.janzen80@gmail.com>
2025-12-13 20:51:45 +0100

    a simple working package

---
+++ __main__.py
@@ -0,0 +1,12 @@
+"""
+Docstring for tuxman.__main__
+"""
+
+def main():
+    print("Hello from my_package!")
+    # Import and use modules from the core package
+    from core import module1
+    module1.some_function()
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,15 @@
+"""
+Docstring for tuxman.setup
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='tuxman',
+    version='0.1',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'tuxman = __main__:main',
+        ],
+    },
+)
