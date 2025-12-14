5de033aae92b30ebb97c964685da891ac54fa5ff
OmVajrapaniHum <jakob.janzen80@gmail.com>
2025-12-14 13:04:10 +0100

    added config

--- __main__.py
+++ __main__.py
@@ -2,21 +2,28 @@
 Docstring for tuxman.__main__
 """
 
-from core import module1
 import os
 import os.path
 
-# from core import sudo
+from core import module1
 from core import sudo
+
+
+CWD = os.getcwd()
+SCRIPT_LOCATION = os.path.dirname(__file__)
+CONFIG_PATH = os.path.join(SCRIPT_LOCATION, "config.json")
+RESOURCES_PATH = os.path.join(SCRIPT_LOCATION, "resources")
 
 
 def main():
     sudo.elevate_privileges()
-    print(f"UID: {os.getuid()}")
     print("Hello from my_package!")
     # Import and use modules from the core package
     module1.some_function()
     print("Current working directory:", os.getcwd())
+    print("Script location:", os.path.dirname(__file__))
+    os.chdir(os.path.dirname(__file__))
+    print("Changed working directory to script location:", os.getcwd())
 
 
 if __name__ == "__main__":
--- core/sudo.py
+++ core/sudo.py
@@ -8,6 +8,8 @@ import sys
 
 def elevate_privileges():
     """Elevate privileges using sudo if not running as root."""
+    print(f"UID before: {os.getuid()}")
     if os.getuid() != 0:
         cmd = ("sudo", sys.executable, *sys.argv)
         os.execvp("sudo", cmd)
+        print(f"UID after: {os.getuid()}")
